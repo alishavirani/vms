127618c16e54d69c2274ccf6336848942c671343
Alisha Virani <av.alisha99@gmail.com>
2021-01-15 00:08:16 +0530

    added route for volunteer contact details

--- controller/volunteer.js
+++ controller/volunteer.js
@@ -9,9 +9,12 @@ const config = require("../config");
 const marital_status_enum = ['Single/Unmarried', 'Married', 'Divorced', 'Widowed', 'Separated'];
 const gender_enum = ['Male', 'Female'];
 const blood_group_enum = ['A+ve', 'A-ve', 'B+ve', 'B-ve', 'AB+ve', 'AB-ve', 'O+ve', 'O-ve'];
+const relation_enum = ['Spouse', 'Father/Mother', 'Brother/Sister', 'Guardian'];
+const volunteerPersonalDetailsTable = "volunteer_personal_details";
+const volunteerContactDetailsTable = "volunteer_contact_details";
 
 //Add personal details of a volunteer route
-router.post('/add-volunteer',async (req, res) => {
+router.post('/add-volunteer', async (req, res) => {
     //validations
     if (!req.body || !req.body.jamatkhana_id || !req.body.first_name || !req.body.last_name || !req.body.dob) {
         res.status(400).send({ "message": "Missing values in the request body. Required fields: jamatkhana_id, first_name, last_name, dob" });
@@ -35,7 +38,7 @@ router.post('/add-volunteer',async (req, res) => {
             return;
         }
     }
-    
+
     try {
         //check if token is valid
         let token = req.headers["authorization"];
@@ -66,8 +69,8 @@ router.post('/add-volunteer',async (req, res) => {
             middle_name: req.body.middle_name || null,
             tkn_number: req.body.tkn_number || null,
             marital_status: req.body.marital_status || "null",
-            gender: req.body.gender  || "null",
-            blood_group: req.body.blood_group  || "null",
+            gender: req.body.gender || "null",
+            blood_group: req.body.blood_group || "null",
             dob: req.body.dob || null,
             active: req.body.active || null,
             created_by: decodedToken.userid,
@@ -76,8 +79,8 @@ router.post('/add-volunteer',async (req, res) => {
 
         for (const column in idsInBody) {
             if (idsInBody[column]) {
-                const tableName = column.slice(0, -3)
-                const found = await volunteer.checkIfRecordExists(tableName, idsInBody[column])
+                const tableName = column.slice(0, -3);
+                const found = await volunteer.checkIfRecordExists(tableName, idsInBody[column]);
                 if (!found) {
                     res.status(404).send({ "message": `ID ${idsInBody[column]} not found in ${tableName} table` });
                     return;
@@ -92,12 +95,88 @@ router.post('/add-volunteer',async (req, res) => {
             volunteer_id: response.volunteer_id,
             data: response
         });
-    } catch(err) {
+    } catch (err) {
         if (err.name === "JsonWebTokenError") {
             res.status(401).send(err);
             return;
         }
-        console.log("err???", err)
+        console.log("error:", err)
+        res.status(500).send(err);
+    }
+});
+
+router.post('/:volunteer_id/contact-details', async (req, res) => {
+    //validations
+    if (!req.params) {
+        res.status(400).send({"message": `Please use a query param value /:volunteer_id/contact-details`});
+        return;
+    }
+
+    if (!req.body || !req.body.mobile_number || !req.body.whatsapp_number || !req.body.email_id || !req.body.city_village || !req.body.pincode || !req.body.emergency_contact_number || !req.body.emergency_contact_name) {
+        res.status(400).send({ "message": "Missing values in the request body. Required fields: mobile_number, whatsapp_number, email_id, city_village, pincode, emergency_contact_number, emergency_contact_name" });
+        return;
+    }
+    if (req.body.emergency_contact_relation) {
+        if (!relation_enum.includes(req.body.emergency_contact_relation)) {
+            res.status(400).send({ "message": `Invalid value of emergency_contact_relation. Please select a value from ${relation_enum}` });
+            return;
+        }
+    }
+
+    try {
+        //check if token is valid
+        let token = req.headers["authorization"];
+        let decodedToken;
+        if (token) {
+            token = token.split(' ')[1];
+            decodedToken = jwt.verify(token, config.jwtSecret);
+        }
+        if (!decodedToken) {
+            res.status(400).send({ "message": "Invalid token" });
+            return;
+        }
+
+        const recordExists = await volunteer.checkIfDetailsExists(req.params.volunteer_id, volunteerContactDetailsTable);
+        if (recordExists) {
+            res.status(409).send({ "message": `Contact details already exists for volunteer id ${req.params.volunteer_id}` });
+            return;
+        }
+
+        const dbData = {
+            mobile_number: req.body.mobile_number,
+            alternate_number: req.body.alternate_number || null,
+            whatsapp_number: req.body.whatsapp_number,
+            email_id: req.body.email_id,
+            address: req.body.address || null,
+            state: req.body.state || null,
+            city_village: req.body.city_village,
+            pincode: req.body.pincode,
+            emergency_contact_number: req.body.emergency_contact_number,
+            emergency_contact_name: req.body.emergency_contact_name,
+            emergency_contact_relation: req.body.emergency_contact_relation || "null",
+            created_by: decodedToken.userid,
+            created_at: new Date().toISOString()
+        }
+
+        const found = await volunteer.checkIfVolunteerExists(req.params.volunteer_id);
+        if (!found) {
+            res.status(404).send({ "message": `ID ${req.params.volunteer_id} not found in ${volunteerPersonalDetailsTable} table` });
+            return;
+        }    
+        dbData["volunteer_id"] = req.params.volunteer_id;
+
+        const response = await volunteer.insertVolunteersContactDetails(dbData);
+        res.status(201).send({
+            "message": "Volunteer contact details added successfully",
+            volunteer_id: response.volunteer_id,
+            data: response
+        })
+    } catch (err) {
+        if (err.name === "JsonWebTokenError") {
+            res.status(401).send(err);
+            return;
+        }
+        console.log("error:", err)
         res.status(500).send(err);
     }
 });
--- db/volunteer.js
+++ db/volunteer.js
@@ -14,6 +14,34 @@ module.exports.checkIfRecordExists = async (tableName, id) => {
     }
 }
 
+module.exports.checkIfVolunteerExists = async (id) => {
+    try {
+        const query = `SELECT * FROM volunteer_personal_details where volunteer_id = '${id}';`;
+        const record = await client.query(query);
+        const found = record.rowCount ? true : false;
+        return found;
+    } catch(err) {
+        console.log(`No volunteer with id ${id} exists`, err);
+        throw err;
+    } finally {
+
+    }
+}
+
+module.exports.checkIfDetailsExists = async (id, tableName) => {
+    try {
+        const query = `SELECT * FROM ${tableName} where volunteer_id = '${id}';`;
+        const record = await client.query(query);
+        const found = record.rowCount ? true : false;
+        return found;
+    } catch(err) {
+        console.log(`No volunteer with id ${id} exists`, err);
+        throw err;
+    } finally {
+
+    }
+}
+
 module.exports.insertVolunteersPersonalDetails = async(data) => {
     try {
         const query = `INSERT INTO volunteer_personal_details (jamatkhana_id, jamati_title_id, tkn_number, jamati_designation_id, title_id, first_name, middle_name, last_name, marital_status, gender, blood_group, dob, active, qualification_id, occupation_id, created_at, created_by)
@@ -25,6 +53,24 @@ module.exports.insertVolunteersPersonalDetails = async(data) => {
         }
     } catch (err) {
         console.log("Cannot insert into volunteers_personal_details table", err);
+        throw err;
+    } finally {
+
+    }
+}
+
+module.exports.insertVolunteersContactDetails = async(data) => {
+    try {
+        const query = `INSERT INTO volunteer_contact_details (volunteer_id, mobile_number, alternate_number, whatsapp_number, email_id, address, state, city_village, pincode, emergency_contact_number, emergency_contact_name, emergency_contact_relation, created_at, created_by)
+        VALUES('${data.volunteer_id}','${data.mobile_number}','${data.alternate_number}','${data.whatsapp_number}','${data.email_id}','${data.address}','${data.state}','${data.city_village}','${data.pincode}','${data.emergency_contact_number}','${data.emergency_contact_name}','${data.emergency_contact_relation}','${data.created_at}','${data.created_by}')
+        RETURNING *`;
+        
+        const response = await client.query(query);
+        if (response.rows.length) {
+            return response.rows[0];
+        }
+    } catch (err) {
+        console.log("Cannot insert into volunteer_contact_details table", err);
         throw err;
     } finally {
 
